296b2d46854b57b574cb6ebff88904ec85ef7db7
ShadowGoat128134 <47763210+ShadowGoat128134@users.noreply.github.com>
2019-05-04 10:15:37 -0400

    Added self hit collision

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,33 +1,57 @@
-var s;
-var a;
-function setup() {
-  var cvn = createCanvas(400, 400);
-  cvn.background(0, 0, 0);
-  s = new Snake();
-  a = new Apple();
-  frameRate(scl);
+var s
+var a
+var lpress = false
+var rpress = false
+var upress = false
+var dpress = false
+function setup () {
+  var cvn = createCanvas(400, 400)
+  cvn.background(0, 0, 0)
+  s = new Snake()
+  a = new Apple()
+  frameRate(scl)
   //s.grow();
 }
 
-function draw() {
-  background(51);
-  a.update();
-  s.update();
+function draw () {
+  background(51)
+  a.update()
+  s.update()
   // Checks for death and resets
-  document.getElementById("score").innerHTML = s.size;
+  document.getElementById('score').innerHTML = s.size
   if (s.die() === true) {
-    s.reset();
+    s.reset()
+    lpress = false
+    rpress = false
+    upress = false
+    dpress = false
   }
 }
 // Key presses
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -spd);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, spd);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(spd, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-spd, 0);
+function keyPressed () {
+  if (keyCode === UP_ARROW && dpress === false) {
+    s.dir(0, -spd)
+    upress = true
+    dpress = false
+    lpress = false
+    rpress = false
+  } else if (keyCode === DOWN_ARROW && upress === false) {
+    s.dir(0, spd)
+    upress = false
+    dpress = true
+    lpress = false
+    rpress = false
+  } else if (keyCode === RIGHT_ARROW && lpress === false) {
+    s.dir(spd, 0)
+    upress = false
+    dpress = false
+    lpress = false
+    rpress = true
+  } else if (keyCode === LEFT_ARROW && rpress === false) {
+    s.dir(-spd, 0)
+    upress = false
+    dpress = false
+    lpress = true
+    rpress = false
   }
-} 
+}
--- docs/snake.js
+++ docs/snake.js
@@ -1,87 +1,96 @@
-var scl = 10;
-var spd = 1;
+var scl = 10
+var spd = 1
 
-function Snake() {
+function Snake () {
+  this.total = 1
+  this.x = [scl]
+  this.y = [scl]
+  var segmentx = []
+  var segmenty = []
+  this.x.speed = 0
+  this.y.speed = 0
+  this.size = this.x.length
+  var selfhit = false
 
-	this.total = 1;
-	this.x = [scl];
-	this.y = [scl];
-	this.x.speed = 0;
-	this.y.speed = 0;
-	this.size = this.x.length;
+  this.dir = function (x, y) {
+    this.x.speed = x * scl
+    this.y.speed = y * scl
+  }
 
+  this.move = function () {
+    if (this.x.length > 1) {
+      var i
+      for (i = this.x.length - 1; i > 0; i--) {
+        this.x[i] = this.x[i - 1]
+        this.y[i] = this.y[i - 1]
+      }
+    }
+    this.x[0] += this.x.speed
+    this.y[0] += this.y.speed
+    segmentx = this.x.slice(1)
+	segmenty = this.y.slice(1)
+  }
 
-	this.dir = function (x, y) {
-		this.x.speed = x * scl;
-		this.y.speed = y * scl;
-	}
+  this.update = function () {
+    this.move()
+    this.eat()
+    fill(255)
+    var i
+    var j
+    for (j = segmentx.length; j >= 0; j--) {
+      if (segmentx[j] === this.x[0] && segmenty[j] === this.y[0]) {
+        selfhit = true
+      }
+    }
+    for (i = this.x.length - 1; i >= 0; i--) {
+      rect(this.x[i], this.y[i], scl, scl)
+    }
+  }
 
-	this.move = function () {
-		if (this.x.length > 1) {
-			var i;
-			for (i = this.x.length - 1; i > 0; i--) {
-				this.x[i] = this.x[i - 1];
-				this.y[i] = this.y[i - 1];
-			}
-		}
-		this.x[0] += this.x.speed;
-		this.y[0] += this.y.speed;
-	}
+  this.die = function () {
+    if (this.x[0] === width || this.x[0] === -scl) {
+      return true
+    } else if (this.y[0] === height || this.y[0] === -scl) {
+      return true
+    } else if (selfhit === true) {
+      return true
+    }
+  }
 
-	this.update = function () {
-		this.move();
-		this.eat();
-		fill(255);
-		var i;
-		for (i = this.x.length - 1; i >= 0; i--) {
-			rect(this.x[i], this.y[i], scl, scl);
-		}
-	}
+  this.reset = function () {
+    this.x = [scl]
+    this.y = [scl]
+    segmentx = []
+    segmenty = []
+    selfhit = false
+    this.dir(0, 0)
+    this.size = this.x.length
+    this.total = 1
+    a.repos()
+  }
 
-	this.die = function () {
-		if (this.x[0] === width || this.x[0] === -scl) {
-			return true;
-		} else if (this.y[0] === height || this.y[0] === -scl) {
-			return true;
-		} 
-	}
+  this.eat = function () {
+    d = dist(this.x[0], this.y[0], a.x, a.y)
+    if (d < 1) {
+      a.repos()
+      this.x.push(this.x[this.x.length - 1] + this.x.speed)
+      this.y.push(this.y[this.y.length - 1] + this.y.speed)
+      this.size++
+    }
+  }
+}
 
-	this.reset = function () {
-		this.x = [scl];
-		this.y = [scl];
-		this.dir(0,0);
-		this.size = this.x.length;
-		this.total = 1;
-		a.repos();
-	}
+function Apple () {
+  this.x = Math.ceil(Math.random() * (width - scl) / scl) * scl
+  this.y = Math.ceil(Math.random() * (height - scl) / scl) * scl
 
-	this.eat = function () {
-		d = dist(this.x[0], this.y[0], a.x, a.y);
-		if (d < 1) {
-			a.repos();
-				this.x.push(this.x[this.x.length - 1] + this.x.speed);
-				this.y.push(this.y[this.y.length - 1] + this.y.speed);
-				this.size ++;
-			}
-		}
-	}
+  this.update = function () {
+    fill(200, 0, 200)
+    rect(this.x, this.y, scl, scl)
+  }
 
-
-
-
-function Apple() {
-
-	this.x = Math.ceil(Math.random() * (width - scl) / scl) * scl;
-	this.y = Math.ceil(Math.random() * (height - scl) / scl) * scl;
-
-	this.update = function () {
-		fill(200, 0, 200);
-		rect(this.x, this.y, scl, scl);
-	}
-
-	this.repos = function () {
-		this.x = Math.ceil(Math.random() * (width - scl) / scl) * scl;
-		this.y = Math.ceil(Math.random() * (height - scl) / scl) * scl;
-	}
-
+  this.repos = function () {
+    this.x = Math.ceil(Math.random() * (width - scl) / scl) * scl
+    this.y = Math.ceil(Math.random() * (height - scl) / scl) * scl
+  }
 }
